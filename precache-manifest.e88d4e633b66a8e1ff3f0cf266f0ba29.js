8cfab114d47031a809a07e38aa5813da1f84cf09
ssuchir <ssuchir@gmail.com>
2020-02-17 14:47:48 -0500

    Updates

---
+++ precache-manifest.e88d4e633b66a8e1ff3f0cf266f0ba29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40e5f23cf42b94e43e42a1875029c103",
+    "url": "/React-redux-songs/index.html"
+  },
+  {
+    "revision": "e9d1bc60791aeb8d084a",
+    "url": "/React-redux-songs/static/css/main.620965ef.chunk.css"
+  },
+  {
+    "revision": "ce17dfbece9646a212e5",
+    "url": "/React-redux-songs/static/js/2.f1bd3c06.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/React-redux-songs/static/js/2.f1bd3c06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9d1bc60791aeb8d084a",
+    "url": "/React-redux-songs/static/js/main.8ef6eb3c.chunk.js"
+  },
+  {
+    "revision": "f5f1a6b986bb42791db5",
+    "url": "/React-redux-songs/static/js/runtime-main.1f1fcb08.js"
+  }
+]);
